56a857a196152a76bc7dcb234678aa7d5431aa3e
khouloud <khouloud.bentaoues33@gmail.com>
2020-05-11 21:51:05 +0100

    got botResponse

--- src/app/angular-bot/chat.service.ts
+++ src/app/angular-bot/chat.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject, throwError } from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {catchError, map} from 'rxjs/operators';
+import { AbstractExtendedWebDriver } from 'protractor/built/browser';
 
 
 export class Message {
@@ -20,36 +21,28 @@ export class ChatService {
   conversation = new Subject<Message[]>();
 
 
-  getBotAnswer(msg: string) {
+
+  async getBotAnswer(msg: string) {
     const userMessage = new Message('user', msg);
     this.conversation.next([userMessage]);
-    const botMessage = new Message('bot', this.getBotMessage(msg));
+    const botMessage = new Message('bot', await this.getBotMessage(msg));
 
-    // setTimeout(() => {
-    //   this.conversation.next([botMessage]);
-    // }, 1500);
+    setTimeout(() => {
+      this.conversation.next([botMessage]);
+    }, 1500);
   }
 
-  getBotMessage(question: string) {
-    // let answer = this.messageMap[question];
-    // return answer || this.messageMap['default'];
-    // tslint:disable-next-line: prefer-const
-    let dataToSend = JSON.stringify({
-      recipient_id: 'user',
-      message: question
-  }) ;
-    let answer = 'error';
-    let resp : Response;
+ async getBotMessage(question: string) {
+  const dataToSend = JSON.stringify({
+    recipient_id: 'user',
+    message: question
+}) ;
+  let answer = 'error';
 
-    this.http.post(this.url, dataToSend).subscribe(data => resp = {
-      recipient_id: (data as any).recipient_id,
-      text:  (data as any).text,
-    });
+  await this.http.post<Response[]>(this.url, dataToSend).toPromise().then( res => answer = res[0].text);
+  return answer;
 
-    return resp.text;
-
+}
 }
 
 
-
-}
